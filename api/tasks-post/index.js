054ed3d6ddc7a044596fa37f7b66ef93da2d0edb
LeFodeurCou <lefodeurcou@gmail.com>
2021-10-27 17:16:05 +0200

    Try to add label to database

--- api/tasks-post/index.js
+++ api/tasks-post/index.js
@@ -6,33 +6,28 @@ module.exports = async function (context, req) {
 	const decoded = encoded.toString('ascii');
 	const user = JSON.parse(decoded);
 
-	var POST = {};
-    if (req.method == 'POST') {
-        req.on('data', function(data) {
-            data = data.toString();
-            data = data.split('&');
-            for (var i = 0; i < data.length; i++) {
-                var _data = data[i].split("=");
-                POST[_data[0]] = _data[1];
-            }
-            console.log(POST);
-        })
+	if (req.body.label)
+	{
+		const client = await mongoClient.connect(process.env.CONNECTION_STRING);
+		const database = client.db(process.env.DATABASE_NAME);
 
-	// const client = await mongoClient.connect(process.env.CONNECTION_STRING);
-	// const database = client.db(process.env.DATABASE_NAME);
-
-	// const response = await database.collection("tasks").insertOne(
-	// 	{
-	// 		"userId": "c807f49263e340b2b4d4e40241d0bae8",
-	// 		"label": "test",
-	// 		"status": ""
-	// 	  }
-	// );
+		const response = await database.collection("tasks").insertOne(
+			{
+				"userId": user.userId,
+				"label": req.body.label,
+				"status": ""
+			}
+		);
+		context.res = {
+			// status: 200, /* Defaults to 200 */
+			body: req.body
+		};
+	}
 	
 	// const tasks = await response.toArray();
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: "response"
+        status: 500,
+        body: "Label isn't provided"
     };
 }
