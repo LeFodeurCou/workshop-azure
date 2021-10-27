0d2f7e91f90c2b338e177e8bf685e92be6e7572e
LeFodeurCou <lefodeurcou@gmail.com>
2021-10-27 16:33:18 +0200

    Try to use MongoDB througt azure 'secrets' (static web app -> configuration)

--- api/tasks-get/index.js
+++ api/tasks-get/index.js
@@ -1,23 +1,39 @@
+const mongoClient = require("mongodb").MongoClient;
+
+const header = req.headers['x-ms-client-principal'];
+const encoded = Buffer.from(header, 'base64');
+const decoded = encoded.toString('ascii');
+const user = JSON.parse(decoded);
+
 module.exports = async function (context, req) {
 	context.log('JavaScript HTTP trigger function processed a request with some tests.');
 
-	const tasks = [
-		{
-			id: 1,
-			label: "🍔 Eat",
-			status: ""
-		},
-		{
-			id: 2,
-			label: "🛏 Sleep",
-			status: ""
-		},
-		{
-			id: 3,
-			label: "</> Code",
-			status: ""
-		}
-	];
+	const client = await mongoClient.connect(process.env.CONNECTION_STRING);
+	const database = client.db(process.env.DATABASE_NAME);
+
+	const response = await database.collection("tasks").find({
+		userId: user.userId
+	});
+	
+	const tasks = await response.toArray();
+
+	// const tasks = [
+	// 	{
+	// 		id: 1,
+	// 		label: "🍔 Eat",
+	// 		status: ""
+	// 	},
+	// 	{
+	// 		id: 2,
+	// 		label: "🛏 Sleep",
+	// 		status: ""
+	// 	},
+	// 	{
+	// 		id: 3,
+	// 		label: "</> Code",
+	// 		status: ""
+	// 	}
+	// ];
 
     context.res = {
         // status: 200, /* Defaults to 200 */
